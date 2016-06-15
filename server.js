da0ebf882a58d61215f4d6bde4b0defab2c03b1b
slfweb <slf.pro@gmail.com>
2016-06-15 19:39:15 +0000

    completed

--- server.js
+++ server.js
@@ -2,14 +2,9 @@
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
 
 var app = express();
 require('dotenv').load();
-
-mongoose.connect(process.env.MONGO_URI);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 

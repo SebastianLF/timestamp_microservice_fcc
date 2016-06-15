411af8d3509f5bd21f3b675331d6028b9cb4f1da
slfweb <slf.pro@gmail.com>
2016-06-15 19:31:38 +0000

    completed

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,32 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var moment = require("moment");
 
 module.exports = function (app, passport) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
+	function unixToTimestamp(date) {
+		return moment.unix(date).format('LL');
 	}
 
-	var clickHandler = new ClickHandler();
-
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
+		.get(function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
 
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+		
+	app.route('/:date')
+		.get(function(req, res) {
+			var date = req.params.date;
+			var dateObject = {};
+			if(!isNaN(date)){
+				dateObject = {unix: date, natural: unixToTimestamp(date)};
+			}
+			else if(moment(date).isValid()){
+				dateObject = {unix: moment(date).unix(), natural: date};
+			}else{
+				dateObject = {unix: null, natural: null};
+			}
+			res.json(dateObject);
+		});	
 };
